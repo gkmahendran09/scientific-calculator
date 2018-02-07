5a568442cf23d41352d874d6afd093bf9bf3ed16
Mahendran GK <gkm09@hotmail.com>
2018-02-08 03:50:56 +0530

    Updated Keypad styles

--- flixbus.com/src/js/components/ScientificCalculator/_Keypad.vue
+++ flixbus.com/src/js/components/ScientificCalculator/_Keypad.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <button @click="$emit('onKeypress', key)" class="keypad__btn" v-for="key in keys">{{key}}</button>
+        <button @click="$emit('onKeypress', key)" class="keypad__btn" :class="getAdditionalClasses(key)" v-for="key in keys">{{key}}</button>
     </div>
 </template>
 <script>
@@ -19,14 +19,34 @@ export default {
 
       ]
     }
-  }
+  },
+
+    methods: {
+      getAdditionalClasses(key) {
+        if(key == "AC" || key == "C") {
+          return "keypad__btn--danger"
+        }
+      }
+    }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
   .keypad__btn {
-    width: 50px;
-    height: 50px;
-    margin: 10px;
+    width: 60px;
+    margin: 5px;
+    background-image: linear-gradient(to bottom, #2E2C30, #4c4c53);
+    color: #fff;
+    border-radius: 6px;
+    font-size: 24px;
+    letter-spacing: -2px;
+    outline: 0;
+    border-bottom-left-radius: 15px;
+    border-bottom-right-radius: 15px;
+    line-height: 35px;
+  }
+
+  .keypad__btn--danger {
+    background-image: linear-gradient(to bottom, #965466, #AC607E);
   }
 </style>
