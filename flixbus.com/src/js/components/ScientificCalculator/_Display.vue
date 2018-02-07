9448fc5bf315c2b8cab93d14592219767c991e99
Mahendran GK <gkm09@hotmail.com>
2018-02-08 03:50:42 +0530

    Updated Display style

--- flixbus.com/src/js/components/ScientificCalculator/_Display.vue
+++ flixbus.com/src/js/components/ScientificCalculator/_Display.vue
@@ -15,22 +15,34 @@ export default {
 </script>
 <style scoped>
 .display {
-  border: 3px solid #636A72;
-  border-radius: 5px;
   width: 100%;
-  padding: 5px;
   background: #D6E3E3;
+  
+  border-radius: 5px;
+
+  /* Border */
+
+  border: 3px solid #636A72;
+
+  border-left-width: 13px;
+  border-right-width: 13px;
+  border-top-width: 10px;
+  border-bottom-width: 20px;
+
+  /* End Border */
 }
 
 .display__input {
   background: inherit;
   width: 100%;
-  line-height: 50px;
-  font-family: 'Orbitron', sans-serif;
-  font-size: 40px;
+  line-height: 75px;
+  font-family: "Digital-7", sans-serif;
+  font-size: 75px;
   direction: rtl;
-  letter-spacing: 5px;
-  border: 0;
+  letter-spacing: -2px;
+  border: 2px solid #46464b;
+  box-shadow: inset #000 2px 2px 2px;
+  padding: 5px;
 }
 </style>
 
