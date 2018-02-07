68ea8dc2dad06829d424a2e7e11cd2eeb9bfb332
Mahendran GK <gkm09@hotmail.com>
2018-02-08 03:51:44 +0530

    Updated the calculation for "ON" / "OFF" state

--- flixbus.com/src/js/components/ScientificCalculator/mixins/calculate.js
+++ flixbus.com/src/js/components/ScientificCalculator/mixins/calculate.js
@@ -2,28 +2,58 @@ const Calculate = {
   methods: {
       // When key pressed
     keyPressed(key) {
+      
+      // Check if the calculator is "ON"
+      // Don't perform any operation
+      // if the calculator is "OFF"
+      if(!this.powerOn && key != "AC") return;
+
+      // Perform the action based on
+      // Key press
       switch (key) {
         case "=":
           this.calculate();
           break;
-        case "C":
         case "AC":
+          this.on();
           this.clear();
+          break;
+        case "C":
+          this.clear();
+          break;
+        case "OFF":
+          this.off();
           break;
 
         default:
-          this.displayText += key;
+          if(this.displayText != "0") {
+            this.displayText += key;
+          } else {
+            this.displayText = key;
+          }
           break;
       }
     },
 
-    // On "="
+
+    // "="
     calculate() {
       this.displayText = eval(this.displayText);
     },
 
-    // On "C" || "AC"
+    // "C" || "AC"
     clear() {
+      this.displayText = "0";
+    },
+
+    // "AC"
+    on() {
+      this.powerOn = true;
+    },
+
+    // "OFF"
+    off() {
+      this.powerOn = false;
       this.displayText = "";
     }
   }
