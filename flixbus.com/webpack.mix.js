8877c91e08045db2ee6ab2138e6632f1567d5139
Mahendran GK <gkm09@hotmail.com>
2018-02-08 03:49:32 +0530

    Added "Digital-7" font

--- flixbus.com/webpack.mix.js
+++ flixbus.com/webpack.mix.js
@@ -3,4 +3,6 @@ let mix = require("laravel-mix");
 mix
   .js("src/js/app.js", "dist/js/")
   .sass("src/css/app.scss", "dist/css/")
+  .copy("src/fonts/", "dist/fonts/")
+  .copy("src/*.html", "dist/")
   .setPublicPath("dist");
