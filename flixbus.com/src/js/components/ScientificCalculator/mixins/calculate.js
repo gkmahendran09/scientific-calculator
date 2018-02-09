c2314d25aca082d43190b856793e57f8d67371b9
Mahendran GK <gkm09@hotmail.com>
2018-02-09 20:38:08 +0530

    Fixed intent

--- flixbus.com/src/js/components/ScientificCalculator/mixins/calculate.js
+++ flixbus.com/src/js/components/ScientificCalculator/mixins/calculate.js
@@ -1,62 +1,75 @@
 const Calculate = {
-  methods: {
-      // When key pressed
-    keyPressed(key) {
+    methods: {
+        // When key pressed
+        keyPressed(key) {
       
-      // Check if the calculator is "ON"
-      // Don't perform any operation
-      // if the calculator is "OFF"
-      if(!this.powerOn && key != "AC") return;
+            // Check if the calculator is "ON"
+            // Don't perform any operation
+            // if the calculator is "OFF"
+            if(!this.powerOn && key != "AC") return;
 
-      // Perform the action based on
-      // Key press
-      switch (key) {
-        case "=":
-          this.calculate();
-          break;
-        case "AC":
-          this.on();
-          this.clear();
-          break;
-        case "C":
-          this.clear();
-          break;
-        case "OFF":
-          this.off();
-          break;
+            // Perform the action based on
+            // Key press
+            switch (key) {
+            case "=":
+                this.calculate();
+                break;
+            case "AC":
+                this.on();
+                break;
+            case "C":
+                this.clear();
+                break;
+            case "OFF":
+                this.off();
+                break;
 
-        default:
-          if(this.displayText != "0") {
-            this.displayText += key;
-          } else {
-            this.displayText = key;
-          }
-          break;
-      }
-    },
+            default:
+                this.updateDisplayText(key);
+                break;
+            }
+        },
 
+        updateDisplayText(key) {
+            if (this.displayText != "0") {
+                if(this.isValidInput(key)) {
+                    this.displayText += key;
+                }
+            } else {
+                this.displayText = key;
+            }
+        },
 
-    // "="
-    calculate() {
-      this.displayText = eval(this.displayText);
-    },
+        isValidInput(key) {
+            // if(key == "+" || key == "-" || key == "*" || key == "/") {
+            //     this.displayText.slice(-1);
+            // }
 
-    // "C" || "AC"
-    clear() {
-      this.displayText = "0";
-    },
+            return true;
+        },
 
-    // "AC"
-    on() {
-      this.powerOn = true;
-    },
+        // "="
+        calculate() {
+            this.displayText = eval(this.displayText);
+        },
 
-    // "OFF"
-    off() {
-      this.powerOn = false;
-      this.displayText = "";
+        // "C" || "AC"
+        clear() {
+            this.displayText = "0";
+        },
+
+        // "AC"
+        on() {
+            this.powerOn = true;
+            this.clear();
+        },
+
+        // "OFF"
+        off() {
+            this.powerOn = false;
+            this.displayText = "";
+        }
     }
-  }
 };
 
 export default Calculate;
